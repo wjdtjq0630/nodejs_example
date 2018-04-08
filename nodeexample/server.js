0c2b97767fa5b100708d6b45a1044a3c6a8a0fee
wjdtjq0630 <wjdtjq0630@gmail.com>
2018-04-08 15:29:39 +0900

    nodejs tutorialspoint

--- nodeexample/server.js
+++ nodeexample/server.js
@@ -1,4 +1,8 @@
 var http = require('http');
-http.createServer((request, response){
+
+http.createServer(function(request, response){
   console.log('서버 시작');
-}).listen(8080);
+  response.writeHead(200, {'Content-Type': 'text/plain charset=utf-8'});
+  response.end('Hello World\n');
+}).listen(8081);
+console.log('Server running at http://127.0.0.1:8081/');
